05bb964422a401ed42c936987c1785f22e6ab1f4
Enrique Jordá <55798786+enrjorarr@users.noreply.github.com>
2021-01-12 20:15:56 +0100

    Editado fichero bot.py
    Se ha editado el fichero bot.py y se han añadido las variables de entorno en heroku

--- bot.py
+++ bot.py
@@ -4,15 +4,16 @@ from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
+import config
 
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events',app)
+slack_event_adapter = SlackEventAdapter(config.SIGNING_SECRET, '/slack/events',app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=config.SLACK_TOKEN)
 BOT_ID = client.api_call("auth.test")['user_id']
 
 @slack_event_adapter.on('message')
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+#===Secret======
+
+SLACK_TOKEN = os.environ['TOKEN']
+SIGNING_SECRET = os.environ['SIGNING_SECRET']
