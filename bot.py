f9e23de22af9e53c292b4a44bdab0dc09fabcf58
Enrique Jordá <55798786+enrjorarr@users.noreply.github.com>
2021-01-12 20:20:39 +0100

    Editado el fichero bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
 from slackeventsapi import SlackEventAdapter
 import config
 
@@ -10,8 +9,7 @@ import config
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(config.SIGNING_SECRET, '/slack/events',app)
+slack_event_adapter = SlackEventAdapter(config.SIGNING_SECRET, '/slack/events','https://egc-bailon-bot-slack.herokuapp.com/')
 
 client = slack.WebClient(token=config.SLACK_TOKEN)
 BOT_ID = client.api_call("auth.test")['user_id']
@@ -27,5 +25,3 @@ def message(payload):
         client.chat_postMessage(channel=channel_id, text=text)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
